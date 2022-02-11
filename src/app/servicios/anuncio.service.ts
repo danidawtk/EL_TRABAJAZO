2f5d80a4fea81a67d2ba50b58b2c4d18ec2ddcab
danidawtk <75862411+danidawtk@users.noreply.github.com>
2022-02-11 22:06:12 +0100

    Add files via upload

--- src/app/servicios/anuncio.service.ts
+++ src/app/servicios/anuncio.service.ts
@@ -2,8 +2,11 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Anuncio } from '../clases/anuncio';
+import { accesoUsuario, User } from '../clases/user';
 
 const url = 'http://localhost/EL_TRABAJAZO/backend/anuncios/'
+
+
 @Injectable({
   providedIn: 'root'
 })
@@ -17,13 +20,32 @@ export class AnuncioService {
   listarAnuncios(): Observable<any>{
     return this.http.get(url+'list/')
   }
+  listarAnunciosAlim(): Observable<any>{
+    return this.http.get(url+'listAlim/')
+  }
+  listarAnunciosElec(): Observable<any>{
+    return this.http.get(url+'listElec/')
+  }
+  listarAnunciosDeco(): Observable<any>{
+    return this.http.get(url+'listDeco/')
+  }
+  listarAnunciosVehi(): Observable<any>{
+    return this.http.get(url+'listVehi/')
+  }
   buscarAnuncio(entrada:string): Observable<any>{
     return this.http.get(url +'list/'+'?busqueda='+ entrada)
   }
   subirImagen(entrada): Observable<any>{
     return this.http.post(url+'image/', entrada) 
    }
-   eliminarAnuncio(): Observable<any>{
-    return this.http.delete(url)
+  
+  leerTusAnuncios(): Observable<any>{
+    return this.http.get(url+"listTusA/")
+  }
+  editarAnu(anuncio: Anuncio): Observable<any>{
+    return this.http.put(url,anuncio)
+  }
+  borrarAnuncio(idanu: number): Observable<any>{
+    return this.http.delete(url+idanu)
   }
 }
