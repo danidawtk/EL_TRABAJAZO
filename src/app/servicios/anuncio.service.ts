df7b66b3761361add8c837c3bdf90692e22addee
danidawtk <75862411+danidawtk@users.noreply.github.com>
2022-02-20 15:55:27 +0100

    Add files via upload

--- src/app/servicios/anuncio.service.ts
+++ src/app/servicios/anuncio.service.ts
@@ -48,4 +48,7 @@ export class AnuncioService {
   borrarAnuncio(idanu: number): Observable<any>{
     return this.http.delete(url+idanu)
   }
+  subirImagenanu(entrada): Observable<any>{
+    return this.http.post(url+'imageanu/', entrada) 
+   }
 }
